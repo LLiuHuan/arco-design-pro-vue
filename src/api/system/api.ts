5dbd8538ff84298427666817fdf07b7258b9ac51
刘欢 <184555556@qq.com>
2022-01-14 19:07:54 +0800

    feat(system): 增加API管理编辑及删除功能
    1. 增加API管理编辑及删除功能

--- src/api/system/api.ts
+++ src/api/system/api.ts
@@ -19,3 +19,82 @@ export const getApiList = (data) => {
     data,
   });
 };
+
+// @Tags menu
+// @Summary 根据id获取菜单
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body api.GetById true "根据id获取菜单"
+// @Success 200 {string} json "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /menu/getApiById [post]
+export const getApiById = (id) => {
+  return http.request({
+    url: '/v1/api/' + id,
+    method: 'get',
+  });
+};
+
+// @Tags Api
+// @Summary 更新api
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body api.CreateApiParams true "更新api"
+// @Success 200 {string} json "{"success":true,"data":{},"msg":"更新成功"}"
+// @Router /api/updateApi [post]
+export const updateApi = (data) => {
+  return http.request({
+    url: '/v1/api',
+    method: 'put',
+    data,
+  });
+};
+
+// @Tags Api
+// @Summary 创建基础api
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body api.CreateApiParams true "创建api"
+// @Success 200 {string} json "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /api/createApi [post]
+export const createApi = (data) => {
+  return http.request({
+    url: '/v1/api',
+    method: 'post',
+    data,
+  });
+};
+
+// @Tags Api
+// @Summary 删除指定api
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body dbModel.Api true "删除api"
+// @Success 200 {string} json "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /api/deleteApi [post]
+export const deleteApi = (data) => {
+  return http.request({
+    url: '/v1/api',
+    method: 'delete',
+    data,
+  });
+};
+
+// @Tags SysApi
+// @Summary 删除选中Api
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body request.IdsReq true "ID"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
+// @Router /api/deleteApisByIds [delete]
+export const deleteApisByIds = (data) => {
+  return http.request({
+    url: '/v1/api/byIds',
+    method: 'delete',
+    data,
+  });
+};
--- src/views/system/api/index.vue
+++ src/views/system/api/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="search-box"> 选择区 </div>
   <div class="table-box">
+    <a-space class="btn-list pb-2">
+      <a-button type="outline">新增</a-button>
+      <a-button type="outline">删除</a-button>
+    </a-space>
     <a-table
       :data="table.data"
       :pagination="{
@@ -33,20 +37,68 @@
         </a-table-column>
         <a-table-column title="操作">
           <template #cell="{ record }">
-            <a-button>删除</a-button>
+            <a-button type="text" @click="editApi(record)">编辑</a-button>
+            <a-popconfirm
+              content="此操作将永久删除所有角色下该api, 是否继续?"
+              okText="确定"
+              cancelText="取消"
+              @ok="removeApi(record)"
+            >
+              <a-button type="text">删除</a-button>
+            </a-popconfirm>
           </template>
         </a-table-column>
       </template>
     </a-table>
   </div>
+
+  <a-modal width="auto" v-model:visible="model.visible" :mask-closable="false">
+    <template #title> {{ model.title }} </template>
+    <div class="pb-4">
+      <a-alert type="warning">新增API，需要在角色管理内配置权限才可使用</a-alert>
+    </div>
+    <a-form ref="modelForm" :model="model.form" :rules="model.rules">
+      <a-form-item field="path" label="路径">
+        <a-input v-model="model.form.path" placeholder="请输入api路径" />
+      </a-form-item>
+      <a-form-item field="method" label="请求">
+        <a-select
+          v-model:model-value="model.form.method"
+          :style="{ width: '100%' }"
+          placeholder="请选择api请求"
+        >
+          <a-option
+            v-for="item in methodOptions"
+            :key="item.value"
+            :label="`${item.label}(${item.value})`"
+            :value="item.value"
+          />
+        </a-select>
+      </a-form-item>
+      <a-form-item field="apiGroup" label="api分组">
+        <a-input v-model="model.form.apiGroup" placeholder="请输入api分组" />
+      </a-form-item>
+      <a-form-item field="description" label="api简介">
+        <a-input v-model="model.form.description" placeholder="请输入api简介" />
+      </a-form-item>
+    </a-form>
+    <template #footer>
+      <a-space>
+        <a-button @click="closeModel">取 消</a-button>
+        <a-button type="primary" @click="enterModel">确 定</a-button>
+      </a-space>
+    </template>
+  </a-modal>
 </template>
 
 <script lang="ts">
   import { defineComponent, onBeforeMount, reactive, ref, toRefs } from 'vue';
-  import { getApiList } from '@/api/system/api';
+  import { createApi, deleteApi, getApiById, getApiList, updateApi } from '@/api/system/api';
+  import { Message } from '@arco-design/web-vue';
   export default defineComponent({
     name: 'SystemApi',
     setup() {
+      const modelForm = ref();
       const methodOptions = [
         {
           value: 'POST',
@@ -82,6 +134,23 @@
           page: 1,
           pageSize: 10,
         },
+        model: {
+          visible: false,
+          title: '',
+          type: '',
+          form: {
+            path: '',
+            apiGroup: '',
+            method: '',
+            description: '',
+          },
+          rules: {
+            path: [{ required: true, message: '请输入api路径', trigger: 'blur' }],
+            apiGroup: [{ required: true, message: '请输入组名称', trigger: 'blur' }],
+            method: [{ required: true, message: '请选择请求方式', trigger: 'blur' }],
+            description: [{ required: true, message: '请输入api介绍', trigger: 'blur' }],
+          },
+        },
       });
 
       const listApi = async () => {
@@ -116,16 +185,107 @@
         return target && `${target.label}`;
       };
 
+      // region 添加和修改
+      const openDialog = (type: string) => {
+        switch (type) {
+          case 'addApi':
+            state.model.title = '新增Api';
+            break;
+          case 'edit':
+            state.model.title = '编辑Api';
+            break;
+          default:
+            break;
+        }
+
+        state.model.type = type;
+        state.model.visible = true;
+      };
+
+      const editApi = async (row) => {
+        const data = await getApiById(row.ID);
+        state.model.form = data.api;
+        openDialog('edit');
+      };
+
+      const removeApi = async (row) => {
+        await deleteApi(row);
+        Message.success('删除成功!');
+
+        if (state.table.data.length === 1 && state.pageInfo.page > 1) {
+          state.pageInfo.page--;
+        }
+        await listApi();
+      };
+
+      const initForm = () => {
+        modelForm.value.resetFields();
+        modelForm.value.clearValidate();
+        state.model.form = {
+          path: '',
+          apiGroup: '',
+          method: '',
+          description: '',
+        };
+      };
+      const closeModel = () => {
+        initForm();
+        state.model.visible = false;
+      };
+
+      const enterModel = async () => {
+        modelForm.value.validate().then(async (res) => {
+          if (res) {
+            return;
+          }
+
+          switch (state.model.type) {
+            case 'addApi':
+              {
+                await createApi(state.model.form);
+                Message.success('添加成功');
+                await listApi();
+                closeModel();
+              }
+
+              break;
+            case 'edit':
+              {
+                await updateApi(state.model.form);
+                Message.success('修改成功');
+                await listApi();
+                closeModel();
+              }
+              break;
+            default:
+              // eslint-disable-next-line no-lone-blocks
+              {
+                Message.error('未知操作');
+              }
+              break;
+          }
+        });
+      };
+      // endregion
+
       onBeforeMount(() => {
         listApi();
       });
 
       return {
+        modelForm,
         ...toRefs(state),
+        methodOptions,
         setPage,
         setPageSize,
 
         methodFiletr,
+
+        editApi,
+        removeApi,
+
+        closeModel,
+        enterModel,
       };
     },
   });

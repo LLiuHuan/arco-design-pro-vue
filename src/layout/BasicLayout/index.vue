f584bbf1f7b6bb35742e1e419f6fc416c91318ed
刘欢 <184555556@qq.com>
2022-06-29 15:53:52 +0800

    refactor(footer): 固定底部
    1. 固定底部

--- src/layout/BasicLayout/index.vue
+++ src/layout/BasicLayout/index.vue
@@ -6,10 +6,17 @@
       <Header v-bind="headerProps" />
       <Tab />
       <a-layout>
-        <a-layout-content :style="{ marginTop: headerHeight + 'px' }">
+        <a-layout-content
+          :style="{
+            marginTop: headerHeight + 'px',
+            marginBottom: theme.footer.fixed ? theme.footer.height + 'px' : '',
+          }"
+        >
           <Content />
         </a-layout-content>
-        <a-layout-footer>Footer</a-layout-footer>
+        <a-layout-footer style="color: var(--color-text-1)" :style="footerStyle">
+          <Footer />
+        </a-layout-footer>
       </a-layout>
     </a-layout>
     <Setting />
@@ -18,14 +25,14 @@
 
 <script lang="ts" setup>
   import { computed } from 'vue';
-  import { useAppStore, useThemeStore } from '@/store';
+  import { useThemeStore } from '@/store';
   import { useBasicLayout } from '@/composables';
-  import { Sider, Header, Tab, Content, Setting } from '@/layout/common';
+  import { Sider, Header, Tab, Content, Setting, Footer } from '@/layout/common';
 
-  const { mode, headerProps, siderVisible, siderWidth, siderCollapsedWidth } = useBasicLayout();
+  const { headerProps } = useBasicLayout();
 
   const theme = useThemeStore();
-  const app = useAppStore();
+  // const app = useAppStore();
 
   // const headerLeft = computed((): number => {
   //   return app.siderCollapse ? siderCollapsedWidth.value : siderWidth.value;
@@ -40,6 +47,30 @@
       height = 0;
     }
     return height;
+  });
+
+  const footerStyle = computed(() => {
+    if (theme.footer.fixed) {
+      return {
+        position: 'fixed',
+        zIndex: 999,
+        height: `${theme.footer.height}px`,
+        paddingLeft: `${theme.sider.width}px`,
+        transitionDuration: '300ms',
+        transitionTimingFunction: 'ease-in-out',
+        transform: 'translateX(0px)',
+        left: 0,
+        bottom: 0,
+        'flex-shrink': 0,
+        'box-sizing': 'border-box',
+        width: '100%',
+        'transition-property': 'padding-left',
+        backgroundColor: 'var(--color-bg-1)',
+      };
+    }
+    return {
+      height: `${theme.footer.height}px`,
+    };
   });
 </script>
 

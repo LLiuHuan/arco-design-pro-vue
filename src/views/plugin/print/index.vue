32f9ec22297ec05225ca3c8eec193dfdf7d57ceb
刘欢 <184555556@qq.com>
2023-02-10 15:08:38 +0800

    feature(plugin): 添加打印（print-js）

--- src/views/plugin/print/index.vue
+++ src/views/plugin/print/index.vue
@@ -1,17 +1,44 @@
 <template>
-  <h1>Print</h1>
+  <div class="h-full">
+    <a-card title="打印" class="shadow-sm rounded-16px">
+      <a-button type="primary" class="mr-10px" @click="printTable">打印表格</a-button>
+      <a-button type="primary" @click="printImage">打印图片</a-button>
+      <template #actions>
+        <web-site-link
+          label="printJS："
+          link="https://github.com/crabbly/Print.js"
+          class="mt-10px"
+        />
+      </template>
+    </a-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
-  // const wsUrl = 'ws://127.0.0.1:8081/v2/base/test';
-  //
-  // const websocket = new WebSocket(wsUrl);
-  // websocket.onopen = () => {
-  //   console.log('websocket open');
-  // };
-  // websocket.onerror = (error) => {
-  //   console.log('websocket error', error);
-  // };
+  import printJS from 'print-js';
+
+  function printTable() {
+    printJS({
+      printable: [
+        { 姓名: '小红', 年龄: '13', 备注: '小红小红小小红' },
+        { 姓名: '小明', 年龄: '15', 备注: '小明小明小小明' },
+        { 姓名: '小刚', 年龄: '18', 备注: '小刚小刚小小刚' },
+      ],
+      properties: ['姓名', '年龄', '备注'],
+      type: 'json',
+    });
+  }
+  function printImage() {
+    printJS({
+      printable: [
+        'https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a20012a2d4d5b9db43dfc6a01fe508c0.png~tplv-uwbnlip3yd-webp.webp',
+        'https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a20012a2d4d5b9db43dfc6a01fe508c0.png~tplv-uwbnlip3yd-webp.webp',
+      ],
+      type: 'image',
+      header: 'Multiple Images',
+      imageStyle: 'width:100%;',
+    });
+  }
 </script>
 
-<style lang="less" scoped></style>
+<style scoped></style>

a89526822be03f21e75e2539f1d762ede773b163
刘欢 <184555556@qq.com>
2024-03-10 21:33:40 +0800

    feat(refactor-dev): add rollup-plugin-visualizer - [增加打包结果分析]

--- types/env.d.ts
+++ types/env.d.ts
@@ -13,3 +13,28 @@ interface EnvConfig {
   /** 代理标识, 用于拦截地址转发代理(如："/api"，这个和后端路径有无 "/api" 路径没有任何关系) */
   proxy: string;
 }
+
+interface ImportMetaEnv {
+  // region GLOB打包后会生成到_app.config.js文件中，用于项目打包后需要可以支持动态配置的场景
+  /** 项目基本地址 */
+  readonly VITE_GLOB_BASE_URL: string;
+  /** 项目名称 */
+  readonly VITE_GLOB_APP_TITLE: string;
+  // endregion
+
+  /** iconify图标作为组件的前缀 */
+  readonly VITE_ICON_PREFIX: string;
+  /**
+   * 本地SVG图标作为组件的前缀, 请注意一定要包含 VITE_ICON_PREFIX
+   * - 格式 {VITE_ICON_PREFIX}-{本地图标集合名称}
+   * - 例如：icon-local
+   */
+  readonly VITE_ICON_LOCAL_PREFIX: string;
+
+  /** Whether to enable analysis of packaged file size results - [是否开启打包文件大小结果分析] */
+  readonly VITE_VISUALIZER?: 'Y' | 'N';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

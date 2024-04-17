b79b0e5a6bc85974a9de8c1cb49b7c922e66c782
刘欢 <184555556@qq.com>
2024-04-17 17:30:05 +0800

    feat(refactor-dev): Search menu internationalization - [搜索菜单国际化]

---
+++ src/locale/lang/en-US/common.ts
@@ -0,0 +1,11 @@
+export default {
+  okText: 'OK',
+  closeText: 'Close',
+  cancelText: 'Cancel',
+  searchText: 'Search',
+  toggleText: 'Toggle',
+  noDataText: 'No Data',
+
+  inputText: 'Please Input',
+  chooseText: 'Please Choose',
+};

947d5ce9ab13f1968f7e3d5d8532632a6baf75da
刘欢 <184555556@qq.com>
2024-06-15 12:26:33 +0800

    feat(utils): Encapsulate console.log - [封装console.log]

--- src/utils/common/console.ts
+++ src/utils/common/console.ts
@@ -1,10 +1,93 @@
+function sprintf(format: string, ...args: any[]) {
+  return format
+    .replace(/%s/g, () => {
+      // 最后undefined过滤掉
+      return args.shift() ?? '';
+    })
+    .trim();
+}
+
+const prettyLog = () => {
+  // const isProduction = import.meta.env.MODE === 'production';
+  const isProduction = import.meta.env.PROD;
+
+  const prettyPrint = (
+    title: string,
+    color: string,
+    message: any,
+    ...optionalParams: any[]
+  ) => {
+    if (isProduction) return;
+    console.log(
+      `%c ${title} %c ${sprintf(message, optionalParams)} %c`,
+      `background:${color};border:1px solid ${color}; padding: 1px; border-radius: 2px 0 0 2px; color: #fff;`,
+      `border:1px solid ${color}; padding: 1px; border-radius: 0 2px 2px 0; color: ${color};`,
+      'background:transparent',
+    );
+  };
+  const info = (message: any, ...optionalParams: any[]) => {
+    prettyPrint('Info', '#909399', message, ...optionalParams);
+  };
+  const infoTitle = (title: string, message: any, ...optionalParams: any[]) => {
+    prettyPrint(title, '#909399', message, ...optionalParams);
+  };
+  const error = (message: any, ...optionalParams: any[]) => {
+    prettyPrint('Error', '#F56C6C', message, ...optionalParams);
+  };
+  const warning = (message: any, ...optionalParams: any[]) => {
+    prettyPrint('Warning', '#E6A23C', message, ...optionalParams);
+  };
+  const success = (message: any, ...optionalParams: any[]) => {
+    prettyPrint('Success', '#67C23A', message, ...optionalParams);
+  };
+  const picture = (url: string, scale = 1) => {
+    if (isProduction) return;
+    const img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.onload = () => {
+      const c = document.createElement('canvas');
+      const ctx = c.getContext('2d');
+      if (ctx) {
+        c.width = img.width;
+        c.height = img.height;
+        ctx.fillStyle = 'red';
+        ctx.fillRect(0, 0, c.width, c.height);
+        ctx.drawImage(img, 0, 0);
+        const dataUri = c.toDataURL('image/png');
+
+        console.log(
+          `%c sup?`,
+          `font-size: 1px;
+                    padding: ${Math.floor((img.height * scale) / 2)}px ${Math.floor((img.width * scale) / 2)}px;
+                    background-image: url(${dataUri});
+                    background-repeat: no-repeat;
+                    background-size: ${img.width * scale}px ${img.height * scale}px;
+                    color: transparent;
+                    `,
+        );
+      }
+    };
+    img.src = url;
+  };
+
+  return {
+    info,
+    infoTitle,
+    error,
+    warning,
+    success,
+    picture,
+  };
+};
+
 /**
  * @description Print log - [打印log]
  * @param message
  * @param optionalParams
  */
 export function consoleLog(message?: any, ...optionalParams: any[]) {
-  console.log(message, ...optionalParams);
+  const { info } = prettyLog();
+  info(message, ...optionalParams);
 }
 
 /**
@@ -13,7 +96,8 @@ export function consoleLog(message?: any, ...optionalParams: any[]) {
  * @param optionalParams
  */
 export function consoleWarn(message?: any, ...optionalParams: any[]) {
-  console.warn(message, ...optionalParams);
+  const { warning } = prettyLog();
+  warning(message, ...optionalParams);
 }
 
 /**
@@ -22,5 +106,6 @@ export function consoleWarn(message?: any, ...optionalParams: any[]) {
  * @param optionalParams
  */
 export function consoleError(message?: any, ...optionalParams: any[]) {
-  console.error(message, ...optionalParams);
+  const { error } = prettyLog();
+  error(message, ...optionalParams);
 }

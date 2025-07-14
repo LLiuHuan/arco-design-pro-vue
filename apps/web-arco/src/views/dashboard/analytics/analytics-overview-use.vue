2c11a96495ec4d4a91a300427f62ac86759dd624
刘欢 <184555556@qq.com>
2025-07-14 19:49:42 +0800

    fix: 修复已知问题
    修复已知问题
    
    修改部分分析页和工作台

---
+++ apps/web-arco/src/views/dashboard/analytics/analytics-overview-use.vue
@@ -0,0 +1,61 @@
+<script lang="ts" setup>
+import type { EchartsUIType } from '@arco/plugins/echarts';
+
+import { onMounted, ref } from 'vue';
+
+import { EchartsUI, useEcharts } from '@arco/plugins/echarts';
+
+const chartRef = ref<EchartsUIType>();
+const { renderEcharts } = useEcharts(chartRef);
+
+onMounted(() => {
+  renderEcharts({
+    grid: {
+      left: 0,
+      right: 0,
+      top: 10,
+      bottom: 0,
+    },
+    xAxis: {
+      type: 'category',
+      show: false,
+    },
+    yAxis: {
+      show: false,
+    },
+    tooltip: {
+      show: true,
+      trigger: 'axis',
+    },
+    series: [
+      {
+        name: '2001',
+        data: Array.from({ length: 12 }, () => Math.floor(Math.random() * 100)),
+        type: 'line',
+        showSymbol: false,
+        smooth: true,
+        lineStyle: {
+          color: '#165DFF',
+          width: 3,
+        },
+      },
+      {
+        name: '2002',
+        data: Array.from({ length: 12 }, () => Math.floor(Math.random() * 100)),
+        type: 'line',
+        showSymbol: false,
+        smooth: true,
+        lineStyle: {
+          color: '#6AA1FF',
+          width: 3,
+          type: 'dashed',
+        },
+      },
+    ],
+  });
+});
+</script>
+
+<template>
+  <EchartsUI ref="chartRef" height="60px" width="100px" />
+</template>

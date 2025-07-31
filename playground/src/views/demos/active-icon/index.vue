528e21853222cc2ce8d45d5050549302ed78ddfd
刘欢 <184555556@qq.com>
2025-08-01 02:05:27 +0800

    feat: 增加自己菜单激活图标示例

---
+++ playground/src/views/demos/active-icon/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { Fallback } from '@arco/common-ui';
+</script>
+
+<template>
+  <Fallback
+    description="用于菜单激活显示不同的图标"
+    status="coming-soon"
+    title="激活图标示例"
+  />
+</template>

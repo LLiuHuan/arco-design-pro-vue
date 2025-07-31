9a6d2c180db645917c9771965f9dafce42dd5eda
刘欢 <184555556@qq.com>
2025-08-01 02:32:15 +0800

    feat: 增加前端权限示例

---
+++ playground/src/views/demos/access/menu-visible-403.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Description: 
+ * @Author: LLiuHuan
+ * @Date: 2025-08-01 02:16:49
+ * @LastEditTime: 2025-08-01 02:18:17
+ * @LastEditors: LLiuHuan
+-->
+<script lang="ts" setup>
+import { Fallback } from '@arco/common-ui';
+</script>
+
+<template>
+  <Fallback
+    description="当前页面用户不可见，会被重定向到403页面"
+    status="coming-soon"
+    title="页面访问测试"
+  />
+</template>
